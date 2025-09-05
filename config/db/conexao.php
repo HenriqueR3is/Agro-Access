8fadea500912edafca09e8d20aba0cdd517d3040
Henrique Hiroshi Koshiba Reis <henriquereis@usacucar.com.br>
2025-09-05 14:33:52 -0300

    refactor: sync bruno archives

--- config/db/conexao.php
+++ config/db/conexao.php
@@ -6,7 +6,7 @@ $user = "root";
 $password = "";
 
 try {
-    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4", $user, $password);
+    $pdo = new PDO("mysql:charset=utf8mb4;host=$host;port=$port;dbname=$dbname;charset=utf8mb4", $user, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Erro de conexão: " . $e->getMessage());
